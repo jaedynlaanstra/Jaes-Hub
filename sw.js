5f75589e8797fbb1da67ecac5a61eadee250eadf
Jaedyn Laanstra <ltljaelaa@gmail.com>
2026-09-22 20:23:03 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Keeps the hub opening even with no signal, and picks up new versions when online.
-var CACHE='hub-v16';
+var CACHE='hub-v17';
 var CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 
 self.addEventListener('install',function(e){
